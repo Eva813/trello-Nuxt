971521ddb36f8c36898212b6f409b6c2325d99b9
Eva <as45986@gmail.com>
2024-03-09 15:59:07 +0800

    fix:imagePicker component

--- components/ImagePicker.vue
+++ components/ImagePicker.vue
@@ -3,10 +3,12 @@ const {
   public: { pixabayApiKey },
 } = useRuntimeConfig();
 
+// 利用雙向綁定的方式，將選擇的圖片傳遞到父元件
 defineProps<{
   modelValue?: string;
 }>();
 
+// 取得圖片列表
 const { data } = await useFetch<any>(
   `https://pixabay.com/api/?key=${pixabayApiKey}&image_type=photo&orientation=horizontal&per_page=32`
 );
@@ -16,25 +18,17 @@ defineEmits<{
 }>();
 </script>
 <template>
-  <ol
-    v-if="data.hits"
-    class="grid grid-cols-4 gap-2 overflow-y-auto overflow-x-auto h-64 p-2"
-  >
-    <li
-      v-for="image in data.hits"
+  <ol v-if="data.hits" class="grid grid-cols-4 gap-2 overflow-y-auto overflow-x-auto h-64 p-2">
+    <!-- 當選到的圖片是 image.largeImageURL 時，會套用 ring-2 ring-blue-500 shadow 的樣式 -->
+    <li v-for="image in data.hits"
       class="h-12 relative rounded overflow-hidden cursor-pointer outline outline-transparent focus:outline-blue-500 hover:outline-blue-500"
       :class="{
         'ring-2 ring-blue-500 shadow': modelValue === image.largeImageURL,
-      }"
-      :key="image.id"
-      @click="$emit('update:modelValue', image.largeImageURL)"
-    >
-      <NuxtImg
-        :src="image.previewURL"
-        class="w-full h-full absolute object-cover"
-      ></NuxtImg>
+      }" :key="image.id" @click="$emit('update:modelValue', image.largeImageURL)">
+      <NuxtImg :src="image.previewURL" class="w-full h-full absolute object-cover"></NuxtImg>
     </li>
   </ol>
 </template>
 
-<style></style>
+<style>
+</style>
